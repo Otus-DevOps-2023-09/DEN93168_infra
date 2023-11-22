f63501e630d96ada5424d668ca26e4e155ccb884
CHDN <den93168@gmail.com>
2023-11-22 11:44:46 +0300

    ansible-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-      version = "0.89.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#      version = "0.89.0"
+#    }
+#  }
+#}
 
 provider "yandex" {
 #  token = "y0_AgAAAAAWejyQAATuwQAAAADk_g5kVSxJNM-1Ttqui1ssPSNIkVtIdRg"
